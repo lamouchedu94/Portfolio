906705f0c2d1dea7ff943f9ae4920a31eeb3fd84
lamouchedu94 <lamouchedu54@gmail.com>
2023-10-06 23:04:55 +0200

    [ADD] User Gestion on url

--- PortfolioPaul/header.php
+++ PortfolioPaul/header.php
@@ -3,6 +3,17 @@ $servername = "localhost";
 $username = "paul";
 $password = "3UhyXkth";
 
+$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+$url_components = parse_url($actual_link);
+// echo $url_components["scheme"]. "://". $url_components["host"] . "/" . $url_components["port"] . "/" . $url_components["path"];
+parse_str($url_components['query'], $params);
+$user_id = $params['id'];
+
+if ($user_id == "") {
+    $user_id = 1;
+}
+
 // try {
 $conn = new PDO("mysql:host=$servername;dbname=Portfolio", $username, $password);
 // set the PDO error mode to exception
@@ -10,7 +21,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $sql = "SELECT * FROM Me WHERE id=?";
 $result = $conn->prepare($sql);
-$user_id = 1;
+// $user_id = 1;
 $result->execute(array($user_id));
 $info = $result->fetch();
 
@@ -24,6 +35,13 @@ foreach ($result as $row) {
     array_push($tabProjects, $row);
 }
 
+$sql = "SELECT COUNT(*) FROM Me";
+$result = $conn->query($sql);
+$numberOfUser = 0;
+foreach ($result as $row) {
+    $numberOfUser = $row;
+}
+$numberOfUser = $numberOfUser[0];
 ?>
 
 
@@ -45,9 +63,14 @@ foreach ($result as $row) {
             <div><h2> <?=$info['firstname'], " ", $info['lastname'];?> </h2></div><a href="tel:+"></a>
 
             <nav class="menu">
-                <div class="option"><h4><a href="./index.php">Home</a></h4></div>
-                <div class="option"><h4><a href="./Projects.php">My Projects</a></h4></div>
-                <div class="option"><h4><a href="./aboutMe.php">About Me</a></h4></div>
+                <div class="option"><h4><a href="./index.php<?="?id=",$user_id?>">Home</a></h4></div>
+                <div class="option"><h4><a href="./Projects.php<?="?id=",$user_id?>">My Projects</a></h4></div>
+                <div class="option"><h4><a href="./aboutMe.php<?="?id=",$user_id?>">About Me</a></h4></div>
+                <button class="switchuser">
+                    <a href="<?="http://". $url_components["host"] . ":" . $url_components["port"] . $url_components["path"] . "?id=" . $user_id%$numberOfUser+1?>" class="switchuserbutton">Switch User</a>
+                    
+                </button>
+               
                 <!-- <ul class="menuContent">
                     <li><a href="./index.php">Home</a></li>
                     <li><a href="./Projects.php">My Projects</a></li>
